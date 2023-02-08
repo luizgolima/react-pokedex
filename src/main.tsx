21bf7e865d5e376546f34f194ffaa6096dea0d33
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-02-08 14:05:38 -0300

    Add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { ResetCss } from "./theme/globalStyles";
 import { ThemeProvider } from "styled-components";
 import { dark } from "./theme";
+import AppRouter from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={dark()}>
       <ResetCss />
-      <App />
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
