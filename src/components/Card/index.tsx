a656a11ebc62b60edb7812c9c8b71ca96755c698
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-02-08 16:45:31 -0300

    Update Card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,14 +1,39 @@
-import { FC } from "react";
+import {FC} from "react";
+// components
+import {FlexBox} from "../Flexbox";
+import * as Atom from "./atoms";
+// types
+import type {ICardProps} from "./types";
+// ::
+const Card: FC<ICardProps> = ({ id, image, name, preview, type }) => {
+  return (
+    <Atom.Container
+      gap="xs"
+      align="center"
+      justify="space-between"
+      direction="column"
+    >
+      <FlexBox align="center" justify="flex-end" direction="row">
+        <Atom.PokemonText type={type}>#{id}</Atom.PokemonText>
+      </FlexBox>
+      <Atom.PokemonSpot
+        type={type}
+        align="center"
+        justify="center"
+        direction="column"
+      >
+        <Atom.PokemonSprite src={image} alt="" />
+      </Atom.PokemonSpot>
+      <Atom.PokemonPreviewSection
+        align="center"
+        justify="space-between"
+        direction="row"
+      >
+        <Atom.PokemonText type={type}>{name}</Atom.PokemonText>
+        {preview && <img src={preview} alt="" />}
+      </Atom.PokemonPreviewSection>
+    </Atom.Container>
+  );
+};
 
-interface ICardProps {
-  name: string;
-  image: string;
-}
-
-const Card: FC<ICardProps> = ({ image, name }) => (
-  <div className="card">
-    {name}
-    <img src={image} alt="" />
-  </div>
-);
 export default Card;
