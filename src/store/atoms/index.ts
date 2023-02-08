8365237b836b420a8fe8576862964b2c357db199
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-02-08 16:49:59 -0300

    Update Store Atom index.ts

--- src/store/atoms/index.ts
+++ src/store/atoms/index.ts
@@ -1,8 +1,27 @@
 import { atom } from "recoil";
+import { IPokemon, IPokemonFetch } from "../../interface";
 
-const atomPokemon = atom({
-  key: "atomPokemon",
-  default: "",
+export const atomPokemonSearch = atom<string | undefined>({
+  key: "atomPokemonSearch",
+  default: undefined,
 });
 
-export default atomPokemon;
+export const atomPokemonFetch = atom<IPokemonFetch[]>({
+  key: "atomPokemonFetch",
+  default: [],
+});
+
+export const atomPokemonOffset = atom<number>({
+  key: "atomPokemonOffset",
+  default: 0,
+});
+
+export const atomPokemonList = atom<IPokemon[]>({
+  key: "atomPokemonList",
+  default: [],
+});
+
+export const atomPokemon = atom<IPokemon>({
+  key: "atomPokemon",
+  default: undefined,
+});
