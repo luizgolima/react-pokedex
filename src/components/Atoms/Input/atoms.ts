2b40032baa0a725bba55baaa8bd409d7af84524b
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-02-08 19:19:25 -0300

    Update atoms.ts

--- src/components/Atoms/Input/atoms.ts
+++ src/components/Atoms/Input/atoms.ts
@@ -9,12 +9,14 @@ export const Input = styled.input`
   box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
   color: ${(props) => props.theme.font.colors.pure};
   width: 100%;
+  outline: none;
+
+  &:hover,
+  &:focus {
+    box-shadow: 0 0 0 1px ${(props) => props.theme.colors.primary};
+  }
 
   ::placeholder {
     color: ${(props) => props.theme.font.colors[1]};
-  }
-
-  :focus {
-    outline: 1px solid ${(props) => props.theme.colors.primary};
   }
 `;
