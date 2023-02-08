79f61e69c62eae3c285f08a46165da9ce04a4d85
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-02-08 15:12:49 -0300

    Update HomePage page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,15 @@
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import atomPokemon from "../../store/atoms";
+import selectorPokemonLength from "../../store/selectors";
 
 const HomePage = () => {
   const [pokemon, setPokemon] = useRecoilState(atomPokemon);
+  const pokemonLength = useRecoilValue(selectorPokemonLength);
   return (
     <div>
       <input type="text" onChange={(event) => setPokemon(event.target.value)} />
       {pokemon}
+      <p>Length: {pokemonLength}</p>
     </div>
   );
 };
