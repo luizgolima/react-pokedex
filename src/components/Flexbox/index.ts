db5c27fa3f77df24c4a141f0f71c38733d72e83c
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-02-08 16:47:21 -0300

    Update Flexbox

--- src/components/Flexbox/index.ts
+++ src/components/Flexbox/index.ts
@@ -1,33 +1,6 @@
 import styled from "styled-components";
-
 // types
-export type TJustifyContent =
-  | "flex-start"
-  | "flex-end"
-  | "center"
-  | "space-between"
-  | "space-around"
-  | "space-evenly"
-  | "initial"
-  | "inherit";
-
-export type TAlignItems =
-  | "stretch"
-  | "center"
-  | "flex-start"
-  | "flex-end"
-  | "baseline"
-  | "initial"
-  | "inherit";
-
-export interface IFlexBoxProps {
-  direction: "row" | "column";
-  justify: TJustifyContent;
-  align: TAlignItems;
-  width?: "xsm" | "sm" | "md" | "lg" | "xl";
-  gap?: "xxxs" | "xxs" | "xs" | "sm" | "md" | "lg" | "xl" | "xxl" | "xxxl";
-  wrap?: "nowrap" | "wrap" | "wrap-reverse" | "initial" | "inherit";
-}
+import type { IFlexBoxProps } from "./types";
 
 export const FlexBox = styled.div<IFlexBoxProps>`
   display: flex;
