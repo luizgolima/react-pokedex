c191d4eea4483121f41a305ddf146940766e7613
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-02-08 15:09:20 -0300

    Create Selector index.ts

---
+++ src/store/selectors/index.ts
@@ -0,0 +1,12 @@
+import { selector } from "recoil";
+import atomPokemon from "../atoms";
+
+const selectorPokemonLength = selector({
+  key: "selectorPokemonLength",
+  get: ({ get }) => {
+    const pokemon = get(atomPokemon);
+    return pokemon.length;
+  },
+});
+
+export default selectorPokemonLength;
