12a003e38a8dcf5e22fd5ecd327be142606e635f
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-02-09 12:14:13 -0300

    Update atoms.ts

--- src/components/Atoms/Header/atoms.ts
+++ src/components/Atoms/Header/atoms.ts
@@ -16,7 +16,7 @@ export const HeaderContainer = styled(FlexBox)`
     padding: ${(props) => props.theme.spacing.xs};
     border-radius: 50%;
     position: relative;
-    left: 29px;
+    left: 28px;
 
     @media (max-width: 576px) {
       display: none;
