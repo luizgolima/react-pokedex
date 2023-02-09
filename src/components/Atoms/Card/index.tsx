98e5c839258316da28fb55be834e11ca4d8cab4f
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-02-09 17:44:37 -0300

    Update Favorite component

--- src/components/Atoms/Card/index.tsx
+++ src/components/Atoms/Card/index.tsx
@@ -1,18 +1,33 @@
-import {FC} from "react";
-// components
+import {FC, useEffect, useState} from "react"; // components
 import {FlexBox} from "../Flexbox";
-import * as Atom from "./atoms";
-// types
-import type {ICardProps} from "./types";
+import * as Atom from "./atoms"; // types
+import type {ICardProps} from "./types"; // ::
 // ::
 
 const Card: FC<ICardProps> = ({ id, image, name, preview, type }) => {
+  const [cardData, setCardData] = useState<ICardProps[]>([]);
+  const [storeFavorite, setStoreFavorite] = useState<ICardProps[]>([]);
+
+  // I want to save an array of objects to localStorage
+  useEffect(() => {
+    if (localStorage.getItem("favorites")) {
+      setStoreFavorite(JSON.parse(localStorage.getItem("favorites") || "[]"));
+    } else {
+      setStoreFavorite([]);
+    }
+    setCardData([...storeFavorite, { id, image, name, preview, type }]);
+  }, [id, image, name, preview, type, storeFavorite]);
+
   return (
     <Atom.Container
       gap="xs"
       align="center"
       justify="space-between"
       direction="column"
+      onClick={() => {
+        localStorage.setItem("favorites", JSON.stringify(cardData));
+        setStoreFavorite(JSON.parse(localStorage.getItem("favorites") || "[]"));
+      }}
     >
       <FlexBox align="center" justify="flex-end" direction="row">
         <Atom.PokemonText type={type}>#{id}</Atom.PokemonText>
--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,7 +1,16 @@
-import { Container, FlexBox } from "../../components";
+import { Card, Container, FlexBox } from "../../components";
 import React from "react";
+import { ICardProps } from "../../components/Atoms/Card/types";
 
 const FavoritesPage = () => {
+  const getFavorites = () => {
+    if (localStorage.getItem("favorites")) {
+      return JSON.parse(localStorage.getItem("favorites") || "[]");
+    } else {
+      return [];
+    }
+  };
+
   return (
     <Container>
       <FlexBox
@@ -10,6 +19,16 @@ const FavoritesPage = () => {
         direction="column"
         gap="xxxs"
       ></FlexBox>
+      {getFavorites()?.map((pokemon: ICardProps) => (
+        <Card
+          key={pokemon.id}
+          id={pokemon.id}
+          image={pokemon.image}
+          name={pokemon.name}
+          preview={pokemon.preview}
+          type={pokemon.type}
+        />
+      ))}
     </Container>
   );
 };
